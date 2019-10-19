49daea6467ea5449d9ef10702100b792b6687e98
Chris Lee-Messer <chris@lee-messer.net>
2019-10-19 10:29:11 -0700

    update to get eegbrowser example to work in jupyter lab

--- notebooks/eegbrowser.py
+++ notebooks/eegbrowser.py
@@ -38,4 +38,23 @@ browser = nb_eegview.EeghdfBrowser(hf)
 # %%
 browser.show()
 
+# %% [markdown]
+# Note, this depends on ipywidgets and bokeh interactions. Depending on the version of ipywidgets and the installation state, this may not work in jupyterlab. For instance, it was not initially working for me in jupyter lab version 1.0.2. However, after doing:
+# ```
+# $ jupyter labextension install @jupyter-widgets/jupyterlab-manager
+# $ jupyter labextension install jupyterlab_bokeh
+# ```
+# It worked fine in jupyter lab for me.
+#
+
+# %%
+bokeh.__version__
+
+# %%
+ipywidgets.__version__
+
+# %%
+import scipy
+scipy.__version__
+
 # %%
