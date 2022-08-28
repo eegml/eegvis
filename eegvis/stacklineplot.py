correct off-by one error on stacklineplot and related functions

eegvis/stacklineplot.py
@@ -10,6 +10,13 @@ import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 
 
+# the idea behind a stacklineplot (from pyeeg) is that we have a bunch of
+# uniformly sampled data in which we want to display one trace a little above the
+# next usually time or samples is the x-axis and we want to be able to make it so
+# the first sample data[0] sits on the leftmost edge of the frame and the last
+# data sample (data[-1]) sits on the right-most edge of the frame
+
+
 def stackplot(
     marray,
     seconds=None,
@@ -70,29 +77,29 @@ def stackplot_t(
     """
     data = tarray
     numSamples, numRows = tarray.shape
-    # data = np.random.randn(numSamples,numRows) # test data
-    # data.shape = numSamples, numRows
+
     if seconds:
-        t = seconds * np.arange(numSamples, dtype=float) / numSamples
-        # import pdb
-        # pdb.set_trace()
+        t = seconds * np.arange(numSamples, dtype=float) / (numSamples - 1)
         if start_time:
             t = t + start_time
             xlm = (start_time, start_time + seconds)
         else:
             xlm = (0, seconds)
-
     else:
-        t = np.arange(numSamples, dtype=float)
-        xlm = (0, numSamples)
+        t = np.arange(numSamples, dtype=float)  # should dtype=int?
+        xlm = (0, numSamples - 1)
 
     # if want to add ability to space by label
     # would do it here, check if labels; make sure right number
     # then interate, use special label to indicate a space
     ticklocs = []
     if not ax:
-        ax = plt.subplot(111)
-
+        ax = plt.subplot(
+            111
+        )  # would it be better to use fig=figure(), then fig.subplots(1,1)?
+    ax.set_xmargin(
+        0
+    )  # may be redundant, not we may be altering settings of an exisiting ax
     ax.set_xlim(*xlm)
     # xticks(np.linspace(xlm, 10))
     dmin = data.min()
@@ -133,7 +140,11 @@ def stackplot_t(
         ylabels.reverse()  # this acts on ylabels in place
     ax.set_yticklabels(ylabels)
 
-    ax.set_xlabel("time (s)")
+    if seconds:
+        ax.set_xlabel("time (s)")
+    else:
+        ax.set_xlabel("sample num")
+
     return ax
 
 
@@ -142,7 +153,24 @@ def test_stacklineplot():
     data = np.random.randn(numRows, numSamples)  # test data
     stackplot(data, 10.0)
 
+def test_stacklineplot0():
+    "using all default arguments"
+    numSamples, numRows = 4, 2
+    data = np.random.randn(numRows, numSamples)  # test data
+    stackplot(data)
 
+
+def test_stacklineplot1():
+    numSamples, numRows = 4, 2
+    data = np.random.randn(numRows, numSamples)  # test data
+    stackplot(data, seconds=3.0)
+
+
+def test_stacklineplot2():
+    numSamples, numRows = 2000, 5
+    data = np.random.randn(numRows, numSamples)  # test data
+    stackplot(data, seconds=10.0)
+    
 def test_stacklineplot_colors():
     numSamples, numRows = 800, 5
     data = np.random.randn(numRows, numSamples)  # test data
@@ -157,6 +185,12 @@ def limit_sample_check(x, signals):
     if x > chan_len:
         return chan_len
     return x
+
+
+# minimal EEG object needs:
+# data: signals : array-like (n_chan, n_samples)
+# sample_frequency
+# optional: channel labels
 
 
 def show_epoch_centered(
@@ -252,7 +286,7 @@ def show_montage_centered(
     # signal_view will not work if channels are not contiguous
     # TODO: use fancy indexing instead?
     signal_view = signals[:, s0:s1]
-    inmontage_view = np.dot(montage.V.data, signal_view)
+    inmontage_view = np.dot(montage.V.data, signal_view) # montage.V.data is matrix (linear transform)
 
     rlabels = montage.montage_labels
     return stackplot(

eegvis/stackplot_bokeh.py
@@ -27,7 +27,7 @@ import eegvis.montageview as montageview
 
 # p = bplt.figure()
 # p.line([1,2,3,4,5], [6,7,2,4,5], line_width=2)
-
+# bokeh.plotting.show(p)  # OR 
 # bplt.show(p)
 
 # Note from http://bokeh.pydata.org/en/latest/docs/user_guide/styling.html
@@ -98,7 +98,7 @@ def stackplot_t(
     # data = np.random.randn(numSamples,numRows) # test data
     # data.shape = numSamples, numRows
     if seconds:
-        t = seconds * np.arange(numSamples, dtype=float) / numSamples
+        t = seconds * np.arange(numSamples, dtype=float) / (numSamples-1)
         # import pdb
         # pdb.set_trace()
         if start_time:
@@ -110,7 +110,7 @@ def stackplot_t(
 
     else:
         t = np.arange(numSamples, dtype=float)
-        # xlm = (0, numSamples)
+        # xlm = (0, numSamples -1)
 
     ticklocs = []
     if "width" not in kwargs:
@@ -464,9 +464,8 @@ class IpyStackplot:
         # data = np.random.randn(numSamples,numRows) # test data
         # data.shape = numSamples, numRows
         if seconds:
-            t = seconds * np.arange(numSamples, dtype=float) / numSamples
-            # import pdb
-            # pdb.set_trace()
+            t = seconds * np.arange(numSamples, dtype=float) / (numSamples-1)
+
             if start_time:
                 t = t + start_time
                 xlm = (start_time, start_time + seconds)
@@ -1035,8 +1034,8 @@ class IpyHdfEegPlot(IpyEEGPlot):
         )
         self.title = "hdf %s - montage: %s" % (
             hdf.filename,
-            self.current_montage.full_name if self.current_montage else "",
-        )
+            self.current_montage.name if self.current_montage else "",
+        ) # try to switch from self.current_montage.full_name to .name
 
 
 class IpyHdfEegPlot2:
@@ -1119,9 +1118,16 @@ class IpyHdfEegPlot2:
         self.ch_stop = self.current_montage.shape[0]
 
     def update_title(self):
+        if 'full_name' in self.current_montage.__dict__:
+            mon_name = self.current_montage.full_name
+        elif 'name' in self.current_montage.__dict__:
+            mon_name = self.current_montage.name
+        else:
+            mon_name = ""
+            
         self.title = "hdf %s - montage: %s" % (
             self.eeghdf_file.hdf.filename,
-            self.current_montage.full_name if self.current_montage else "",
+            mon_name,
         )
 
     #     def __init__(self,